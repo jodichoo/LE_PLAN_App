69ff98126cf0019e85038d754fdc001d493b24a6
Shee Hui <tansheehui@gmail.com>
2021-06-27 16:28:16 +0800

    Add Friends tab

---
+++ src/components/FriendsTab.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useAuth } from "../navigation/AuthProvider";
+import { db } from "../firebase/config";
+
+function FriendsTab() {
+
+}
+
+export default FriendsTab;
