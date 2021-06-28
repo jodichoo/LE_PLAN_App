716bb903f3b970537630b230c966a2d490b5167f
Shee Hui <tansheehui@gmail.com>
2021-06-28 20:04:53 +0800

    Switch firebase config and db

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,14 +15,14 @@ import '@firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDHVEgZAF13qqCmkAs-uoVXEz8t6ockCwQ",
-  authDomain: "wlbbot-c1927.firebaseapp.com",
-  projectId: "wlbbot-c1927",
-  databaseURL: 'https://wlbbot-c1927.firebaseio.com',
-  storageBucket: "wlbbot-c1927.appspot.com",
-  messagingSenderId: "560679851",
-  appId: "1:560679851:web:8b77914fcb57787bc4e0ee",
-  measurementId: "G-B1X10HPT1E"
+  apiKey: "AIzaSyDecEux5fREYrO0x2F0QG8zFSanUD9HylA",
+  authDomain: "wlbbot-fdd9e.firebaseapp.com",
+  projectId: "wlbbot-fdd9e",
+  databaseURL: 'https://wlbbot-fdd9e.firebaseio.com',
+  storageBucket: "wlbbot-fdd9e.appspot.com",
+  messagingSenderId: "142675589929",
+  appId: "1:142675589929:web:1b330ab4e5456354ead538",
+  measurementId: "G-JHG7BKEWQB"
 };
 
 if (!firebase.apps.length) {
