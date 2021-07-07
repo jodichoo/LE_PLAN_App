aa126ad9d55fe96fe86752145aa4b2f5aacad870
Shee Hui <tansheehui@gmail.com>
2021-07-07 15:19:50 +0800

    add custom navigation for logout button

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,21 +1,38 @@
 import React, { useEffect, useState, useContext } from "react";
-import { AuthContext } from "../../navigation/AuthProvider";
+import { AuthContext } from "./AuthProvider";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import SettingsScreen from "../screens/SettingsScreen";
 import { StyleSheet, Text, View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-const AppStack = (props) => {
-  const { currentUser, logout } = useContext(AuthContext);
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
+const AppStack = () => {
+  const { logout } = useContext(AuthContext);
   const Drawer = createDrawerNavigator();
   
+  function CustomDrawerContent(props) {
+    return (
+      <DrawerContentScrollView {...props} contentContainerStyle={{flex: 1, justifyContent: 'space-between'}}>
+        <DrawerContentScrollView>
+        <DrawerItem label={() => <Text style={{ color: 'white', height: 200 }}>Profile image, square</Text>}
+          style={{backgroundColor: 'gray', marginTop: 20}} 
+        />
+        <DrawerItemList {...props} />
+        </DrawerContentScrollView>
+        <DrawerItem label={() => <Text style={{ color: 'white' }}>Logout</Text>}
+          style={{backgroundColor: 'red', marginBottom: 10}} 
+          onPress={logout}
+        />
+      </DrawerContentScrollView>
+    );
+  }
+
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator initialRouteName="Home" drawerContent={props => <CustomDrawerContent {...props} />}>
       <Drawer.Screen name="Profile" component={ProfileScreen} />
-      <Drawer.Screen name="Settings" component={SettingsScreen} />
       <Drawer.Screen name="Home" component={HomeScreen} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
     </Drawer.Navigator>
   );
 };
