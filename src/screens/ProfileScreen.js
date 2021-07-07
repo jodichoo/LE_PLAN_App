66623b3b7e6369d1a336546c08cf68ea36e4710f
Shee Hui <tansheehui@gmail.com>
2021-07-07 12:31:50 +0800

    create profile and settings screen

---
+++ src/screens/ProfileScreen.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState, useContext } from "react";
+import {
+    FlatList,
+    Keyboard,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Button,
+  } from "react-native";
+
+function ProfileScreen() {
+    return (
+        <div><Text>Profile</Text></div>
+    )
+}
+
+export default ProfileScreen;
---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState, useContext } from "react";
+import {
+    FlatList,
+    Keyboard,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Button,
+  } from "react-native";
+
+function SettingsScreen() {
+    return (
+        <div><Text>Settings</Text></div>
+    )
+}
+
+export default SettingsScreen;
