358823de2bc319a3879d1fbcd26e97e7d538286c
Jodi Choo <jodi.choo@gmail.com>
2021-07-12 02:52:16 +0800

    Fix styling for settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -7,6 +7,7 @@ import {
   Modal,
   Image,
   StyleSheet,
+  ScrollView
 } from "react-native";
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase/config";
@@ -24,13 +25,13 @@ function SettingsScreen(props) {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confPassword, setConfPassword] = useState("");
-  const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
   const [changeName, setChangeName] = useState(false);
   const [newName, setNewName] = useState("");
   const [picUrl, setPicUrl] = useState("");
   const [useDefault, setUseDefault] = useState(true);
-  const [urlError, setUrlError] = useState("");
+  const [urlError, setUrlError] = useState('');
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -151,19 +152,19 @@ function SettingsScreen(props) {
 
   function toggleAuth() {
     return (
-      <View>
-        <Text>{error && <Text style={styles.error}>{error}</Text>}</Text>
-        <Text>Confirm Current Password:</Text>
+      <View style={{alignItems: 'center', justifyContent: 'center', width: '100%'}}>
+        {error.length > 0 && <Text style={styles.error}>{error}</Text>}
+        <Text style={styles.text}>Confirm Current Password:</Text>
         <TextInput
           style={styles.input}
           secureTextEntry={true}
           type="password"
           onChangeText={(e) => setOldPassword(e)}
         />
-        <Pressable onPress={toggleUpdate}>
-          <Text>Submit</Text>
+        <Pressable style={styles.setButton} onPress={toggleUpdate}>
+          <Text style={{color: 'whitesmoke'}}>Submit</Text>
         </Pressable>
-        <Pressable
+        {/* <Pressable
           onPress={() => {
             setUrlError("");
             setError("");
@@ -172,7 +173,7 @@ function SettingsScreen(props) {
           }}
         >
           <Text>X</Text>
-        </Pressable>
+        </Pressable> */}
       </View>
     );
   }
@@ -203,127 +204,133 @@ function SettingsScreen(props) {
   }
 
   return (
-    <View style={styles.container}>
-      {/* <View className='back' onClick={history.goBack}><IoChevronBackOutline style={{fontSize: '20px'}}/><text>Back</text></div> */}
-      <View style={styles.msg}>
-        <Text>{success && <Text style={styles.succ}>{success}</Text>}</Text>
-      </View>
+      <View style={styles.container}>
+        <View style={styles.header}><Text style={{fontSize: 48, fontWeight: '700'}}>Settings</Text></View>
+        {/* <View className='back' onClick={history.goBack}><IoChevronBackOutline style={{fontSize: '20px'}}/><text>Back</text></div> */}
+        
+          {success.length > 0 && <View style={styles.msg}>
+            <Text style={styles.succ}>{success}</Text>
+            </View>}
 
-      <View style={styles.msg}>
-        <Text>{urlError && <Text style={styles.error}>{urlError}</Text>}</Text>
-      </View>
+          {urlError > 0 && <View style={styles.msg}>
+            <Text style={styles.error}>{urlError}</Text>
+            </View>}
 
-      {useDefault ? (
-        <Image
-          style={styles.img}
-          source={require("../../assets/defaultProfile.png")}
-        />
-      ) : (
-        <Image
-          style={styles.img}
-          source={{ uri: currentUser.photoURL }}
-          onError={(e) => {
-            setUseDefault(true);
-            setDef(true);
-          }}
-        />
-      )}
+        {useDefault ? (
+          <Image
+            style={styles.img}
+            source={require("../../assets/defaultProfile.png")}
+          />
+        ) : (
+          <Image
+            style={styles.img}
+            source={{ uri: currentUser.photoURL }}
+            onError={(e) => {
+              setUseDefault(true);
+              setDef(true);
+            }}
+          />
+        )}
 
-      <Text>Upload Profile Picture URL</Text>
-      <View style={styles.setPic}>
-        <TextInput
-          style={styles.input}
-          onChangeText={(e) => setPicUrl(e)}
-          placeholder="e.g. pic.png, pic.jpg"
-          value={picUrl}
-        />
-
-        <Pressable onPress={handleSetProfilePic}>
-          <Text>Set Picture</Text>
-        </Pressable>
-      </View>
-      <Text>Your Username: {username}</Text>
-
-      <Text>Your Email: {currentUser.email}</Text>
-
-      <Pressable onPress={() => setConfirmPassP(true)}>
-        <Text>Change Password</Text>
-      </Pressable>
-
-      <View style={styles.msg}>
-        <Text>
-          {error && changePass && <Text style={styles.error}>{error}</Text>}
-        </Text>
-      </View>
-
-      {changePass && (
-        <View>
-          <Text>New password:</Text>
+        <View style={styles.setPic}>
+          <Text style={styles.text}>Upload Profile Picture URL</Text>
           <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            onChangeText={(e) => setNewPassword(e)}
+              style={styles.input}
+              onChangeText={(e) => setPicUrl(e)}
+              placeholder="e.g. pic.png, pic.jpg"
+              value={picUrl}
           />
 
-          <Text>Confirm new password:</Text>
-          <TextInput
-            style={styles.input}
-            secureTextEntry={true}
-            onChangeText={(e) => setConfPassword(e)}
-          ></TextInput>
-
-          <Pressable onPress={handleChangePassword}>
-            <Text>Submit</Text>
-          </Pressable>
-          <Pressable onPress={() => setChangePass(false)}>
-            <Text>Cancel</Text>
+          <Pressable style={styles.setButton} onPress={handleSetProfilePic}>
+            <Text style={{color: 'whitesmoke'}}>Set Picture</Text>
           </Pressable>
         </View>
-      )}
 
-      <Pressable onPress={() => setConfirmPassN(true)}>
-        <Text>Change Display Name</Text>
-      </Pressable>
+        <Text style={styles.text}>Your Username: {username}</Text>
 
-      {changeName && (
-        <View>
-          <Text>New Display Name:</Text>
-          <TextInput style={styles.input} onChangeText={(e) => setNewName(e)} />
+        <Text style={styles.text}>Your Email: {currentUser.email}</Text>
 
-          <Pressable onPress={handleChangeName}>
-            <Text>Submit</Text>
+        <View style={styles.changeCreds}>
+          <Pressable style={{marginVertical: 5}} onPress={() => setConfirmPassP(true)}>
+            <Text style={{...styles.text, color: 'gray', textDecorationLine:'underline'}}>Change Password</Text>
           </Pressable>
-          <Pressable onPress={() => setChangeName(false)}>
-            <Text>Cancel</Text>
+
+          {error.length > 0 && changePass && (<View>
+            <Text style={styles.error}>{error}</Text>
+            </View>)}
+
+          {changePass && (
+            <View style={styles.changeForm}>
+              <Text style={styles.text}>New password:</Text>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                onChangeText={(e) => setNewPassword(e)}
+              />
+
+              <Text style={styles.text}>Confirm new password:</Text>
+              <TextInput
+                style={styles.input}
+                secureTextEntry={true}
+                onChangeText={(e) => setConfPassword(e)}
+              ></TextInput>
+              
+              <View style={{flexDirection: 'row', width: '80%', justifyContent: 'space-evenly'}}>
+                <Pressable style={styles.setButton} onPress={handleChangePassword}>
+                  <Text>Submit</Text>
+                </Pressable>
+                <Pressable style={styles.cancelButton} onPress={() => setChangePass(false)}>
+                  <Text>Cancel</Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
+
+          <Pressable onPress={() => setConfirmPassN(true)}>
+            <Text style={{...styles.text, color: 'gray', textDecorationLine:'underline'}}>Change Display Name</Text>
           </Pressable>
+
+          {changeName && (
+            <View style={styles.changeForm}>
+              <Text style={styles.text}>New Display Name:</Text>
+              <TextInput style={styles.input} onChangeText={(e) => setNewName(e)} />
+              <View style={{flexDirection: 'row', width: '80%', justifyContent: 'space-evenly'}}>
+                <Pressable style={styles.setButton} onPress={handleChangeName}>
+                  <Text>Submit</Text>
+                </Pressable>
+                <Pressable style={styles.cancelButton} onPress={() => setChangeName(false)}>
+                  <Text>Cancel</Text>
+                </Pressable>
+              </View>
+            </View>
+          )}
         </View>
-      )}
-
-      <Modal transparent={true} visible={confirmPassN || confirmPassP}>
-        <TouchableOpacity
-          onPress={() => {
-            setConfirmPassN(false);
-            setConfirmPassP(false);
-          }}
-          style={{ backgroundColor: "#000000aa", flex: 1 }}
-        >
-          {/* to implement touch outside => remove modal */}
+        <Modal transparent={true} visible={confirmPassN || confirmPassP}>
           <TouchableOpacity
-            onPress={() => console.log("")}
-            activeOpacity={1}
-            style={{
-              backgroundColor: "whitesmoke",
-              margin: 50,
-              padding: 40,
-              borderRadius: 10,
-              flex: 1,
+            onPress={() => {
+              setConfirmPassN(false);
+              setConfirmPassP(false);
             }}
+            style={{ backgroundColor: "#000000aa", flex: 1, justifyContent: 'center'}}
           >
-            {toggleAuth()}
+            {/* to implement touch outside => remove modal */}
+            <TouchableOpacity
+              onPress={() => console.log("")}
+              activeOpacity={1}
+              style={{
+                backgroundColor: "whitesmoke",
+                margin: 50,
+                borderRadius: 10,
+                flex: 0.25,
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              {toggleAuth()}
+            </TouchableOpacity>
           </TouchableOpacity>
-        </TouchableOpacity>
-      </Modal>
-    </View>
+        </Modal>
+      </View>
   );
 }
 
@@ -331,10 +338,21 @@ export default SettingsScreen;
 
 const styles = StyleSheet.create({
   container: {
+    width: '100%',
     flex: 1,
     alignItems: "center",
     marginTop: 50,
   },
+  header: {
+    width: '100%',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+    marginBottom: 20,
+  }, 
+  text: {
+    fontSize: 16, 
+    fontWeight: '500',
+  }, 
   img: {
     width: 250,
     height: 250,
@@ -344,20 +362,50 @@ const styles = StyleSheet.create({
     width: 250,
     height: 250,
     borderRadius: 125,
-    marginTop: -250,
   },
   input: {
-    padding: 1,
+    color: 'black',
+    width: '70%',
+    paddingHorizontal: 10,
     borderRadius: 10,
-    height: 20,
     backgroundColor: "rgba(0, 0, 0, 0.05)",
+    height: 30, //typed text only shows with this??
   },
   setPic: {
+    width: '100%',
+    alignItems: 'center',
     paddingTop: 10,
-    paddingBottom: 10,
-    flexDirection: "row",
+    paddingBottom: 20,
+    flexDirection: "column",
+  },
+  setButton: {
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 8,
+    alignItems: 'center',
+    width: '25%',
+    backgroundColor: 'turquoise',
+  },
+  cancelButton: {
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 8,
+    alignItems: 'center',
+    width: '25%',
+    backgroundColor: 'pink',
+  },
+  changeCreds: {
+    width: '100%',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  changeForm: {
+    marginTop: 10,
+    width: '100%',
+    alignItems: 'center',
   },
   msg: {
+    backgroundColor: 'red',
     alignSelf: "center",
     width: "100%",
     paddingHorizontal: 20,
