461f6ccfcb34718c96bbc812611985325074ec70
Jodi Choo <jodi.choo@gmail.com>
2021-07-12 17:11:52 +0800

    Display user profile + reserve space for stonks

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../navigation/AuthProvider";
 import { db } from "../firebase/config";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View, Image } from "react-native";
 
 function ProfileScreen() {
   const { currentUser } = useAuth();
@@ -9,6 +9,20 @@ function ProfileScreen() {
   const [dataSet, setDataSet] = useState([]);
   const [workSet, setWorkSet] = useState([]);
   const [playSet, setPlaySet] = useState([]);
+  const [useDefault, setUseDefault] = useState(false);
+  const [username, setUsername] = useState('default'); 
+  const [loading, setLoading] = useState(true); 
+
+  useEffect(() => {
+    userTasks
+      .get()
+      .then((doc) => {
+        setUsername(doc.data().username); 
+      })
+      .then(() => {
+        setLoading(false); 
+      })
+  }, [])
 
   useEffect(() => {
     userTasks
@@ -70,11 +84,104 @@ function ProfileScreen() {
     return converted;
   }
 
+  function renderImage() {
+    fetch(currentUser.photoURL)
+      .then((res) => {
+        if (res.status == 200) {
+          setUseDefault(false);
+        }
+      })
+      .catch((error) => {
+        setUseDefault(true);
+      });
+    return useDefault ? (
+      <Image
+        style={styles.img}
+        source={require("../../assets/defaultProfile.png")}
+      />
+    ) : (
+      <Image
+        style={styles.img}
+        source={{ uri: currentUser.photoURL }}
+        onError={(e) => {
+          setUseDefault(true);
+        }}
+      />
+    );
+  }
+      
   return (
-    <View style={{ flex: 1, justifyContent: "center" }}>
-      <Text>Profile</Text>
+    <View style={styles.container}>
+      <View style={styles.header}><Text style={{fontSize: 48, fontWeight: '700'}}>Profile</Text></View>
+      {loading || <View style={styles.profile}>
+        
+        <View style={styles.imgContainer}>
+          {renderImage()}
+        </View>
+        <Text style={styles.displayName}>{currentUser.displayName}</Text>
+        <Text style={styles.creds}>
+          Un: {username}
+        </Text>
+        <Text style={styles.creds}>
+          Email: {currentUser.email}
+        </Text>
+      </View>}
+      <View style={styles.stonks}>
+        <Text>Can put the stonks here</Text>
+      </View>
     </View>
   );
 }
 
 export default ProfileScreen;
+
+const styles = {
+  container: { 
+    alignItems: 'center',
+    flex: 1, 
+    marginTop: 50,
+  },
+  header: {
+    width: '100%',
+    justifyContent: 'flex-start',
+    paddingHorizontal: 20,
+  }, 
+  profile: {
+    alignItems: 'center',
+  },
+  displayName: {
+    fontSize: 45,
+    fontWeight: '600'
+  },  
+  imgContainer: {
+    backgroundColor: 'white',
+    width: 200, 
+    height: 200, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+    borderRadius: 200,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  img: {
+    width: 200,
+    height: 200,
+  },
+  creds: {
+    fontWeight: '300',
+    color: 'gray',
+    fontSize: 20,
+  }, 
+  stonks: {
+    width: '88%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+    flex: 1,
+    borderStyle: 'solid',
+    borderColor: 'black', 
+    borderWidth: 1.4, 
+  }
+}
