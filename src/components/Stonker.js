a8bf6dd87714bfb41e2b3cf61d80a61109debb31
Shee Hui <tansheehui@gmail.com>
2021-07-13 15:13:44 +0800

    get stonked

--- src/components/Stonker.js
+++ src/components/Stonker.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useAuth } from "../navigation/AuthProvider";
 import { db } from "../firebase/config";
 import { Text, View } from "react-native";
+import { LineChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
 
 function Stonker() {
   const { currentUser } = useAuth();
   const userTasks = db.collection("users").doc(currentUser.uid);
   const [workSet, setWorkSet] = useState([]);
   const [playSet, setPlaySet] = useState([]);
-  const [dataSet, setDataSet] = useState([]);
+  const [dataSet, setDataSet] = useState([-1,-1,-1,-1,-1]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     userTasks
@@ -35,6 +38,9 @@ function Stonker() {
       .then(() => {
         setWorkSet(convertToWork());
         setPlaySet(convertToPlay());
+      })
+      .then(() => {
+        setLoading(false);
       });
   }, []);
 
@@ -49,7 +55,7 @@ function Stonker() {
         converted.push(arr[i]);
       }
     }
-
+    console.log(converted)
     return converted;
   }
 
@@ -72,7 +78,47 @@ function Stonker() {
 
   return (
     <View>
-      <Text>Stonker graph</Text>
+      {loading || (
+        <LineChart
+          data={{
+            labels: ["", "", "", "", "this week"],
+            datasets: [
+              {
+                data: workSet
+              },
+              {
+                  data: playSet
+              }
+            ],
+          }}
+          width={300} // from react-native
+          height={220}
+          yAxisLabel=""
+          yAxisSuffix="%"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: "#e26a00",
+            backgroundGradientFrom: "#fb8c00",
+            backgroundGradientTo: "#ffa726",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: "6",
+              strokeWidth: "2",
+              stroke: "#ffa726",
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      )}
     </View>
   );
 }
