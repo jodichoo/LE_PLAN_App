9de2395fe8d41675325027ad5843e114e4bdad93
Shee Hui <tansheehui@gmail.com>
2021-07-24 22:03:23 +0800

    add dark theme to calendar + fix display

--- src/components/CalendarItem.js
+++ src/components/CalendarItem.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useContext } from "react";
 import { useAuth } from "../navigation/AuthProvider";
 import { db } from "../firebase/config";
+import { ThemeContext } from "../theme/ThemeContext";
 import moment from "moment";
 import TaskForm from "./TaskForm";
-import Swipeable from 'react-native-swipeable-row'; 
-import { Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import Swipeable from "react-native-swipeable-row";
+import { Octicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import {
   StyleSheet,
   View,
@@ -18,6 +19,7 @@ import {
 
 function CalendarItem(props) {
   const { item, selectedDate, setTriggerLoad } = props;
+  const { dark, theme } = useContext(ThemeContext);
   const { currentUser } = useAuth();
   const userTasks = db.collection("users").doc(currentUser.uid);
   const [edit, setEdit] = useState(false);
@@ -66,90 +68,105 @@ function CalendarItem(props) {
   }
 
   function addDurationAndFormat(time, dur) {
-    var newMoment = moment(); 
-    var h; 
+    var newMoment = moment();
+    var h;
     if (time === 0) {
-      h = time; 
-      newMoment = newMoment.hour(h).minute(0); 
+      h = time;
+      newMoment = newMoment.hour(h).minute(0);
     } else {
       const str = time.toFixed(2);
       const split = str.split(".");
       h = parseInt(split[0]);
       const m = parseInt(split[1]);
-      newMoment = newMoment.hour(h).minute(m); 
+      newMoment = newMoment.hour(h).minute(m);
     }
-    console.log(newMoment.format('HH:mm'));
-    return newMoment.add(dur, 'hours').format('HH:mm');
+    console.log(newMoment.format("HH:mm"));
+    return newMoment.add(dur, "hours").format("HH:mm");
   }
 
   const deleteWarning = () => {
-    Alert.alert(
-      'Delete Task',
-      'Are you sure you want to delete this task?',
-      [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-          onPress: () => console.log('cancel'), 
-        }, 
-        {
-          text: 'Delete', 
-          onPress: () => deleteTask(item),
-        }
-      ]
-    );
-  }
+    Alert.alert("Delete Task", "Are you sure you want to delete this task?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+        onPress: () => console.log("cancel"),
+      },
+      {
+        text: "Delete",
+        onPress: () => deleteTask(item),
+      },
+    ]);
+  };
 
   const rightButtons = [
-    <TouchableHighlight style={styles.delete} onPress={() => {
-      deleteWarning(); 
-    }}>
+    <TouchableHighlight
+      style={styles.delete}
+      onPress={() => {
+        deleteWarning();
+      }}
+    >
       <Text style={styles.buttonText}>Delete</Text>
-    </TouchableHighlight>
-  ]; 
+    </TouchableHighlight>,
+  ];
 
   return (
-    <View style={{marginTop: 8}}>
-    <Swipeable style={{marginTop: 8}} rightButtons={rightButtons}>
+    <View style={{ marginTop: 8 }}>
+      <Swipeable style={{ marginTop: 8 }} rightButtons={rightButtons}>
+        <TouchableOpacity onPress={triggerEdit} style={styles.touchable}>
+          {!edit && (
+            <View style={[styles.container, { borderColor: theme.color }]}>
+              <View style={styles.left}>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    fontWeight: "bold",
+                    color: theme.color,
+                  }}
+                >
+                  {convertTime(item.time)}
+                  {"-"}
+                  {addDurationAndFormat(item.time, item.dur)}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 17,
+                    fontWeight: "bold",
+                    color: theme.color,
+                  }}
+                >
+                  {item.name}
+                </Text>
+                <Text style={{ fontSize: 14, color: theme.color }}>
+                  {item.desc}
+                </Text>
+              </View>
 
-    <TouchableOpacity
-      onPress={triggerEdit}
-      style={styles.touchable}
-    >
-      {!edit && (
-        <View style={styles.container}>
-
-          <View style={styles.left}>
-            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-              {convertTime(item.time)}
-              {"-"}
-              {addDurationAndFormat(item.time, item.dur)}
-            </Text>
-            <Text style={{ fontSize: 17, fontWeight: 'bold' }}>{item.name}</Text>
-            <Text style={{ fontSize: 14 }}>{item.desc}</Text>
-             
-          </View>
-
-          <View style={styles.right}>
-            {item.isWork ? <Octicons name="briefcase" size={30} color="pink" /> : <MaterialCommunityIcons name="gamepad-variant" size={33} color="turquoise" />}
-          </View>
-        </View>
-        
-      )}
-      {edit && (
-        <TaskForm
-          setTriggerLoad={setTriggerLoad}
-          updateCalendar={true}
-          selectedDate={selectedDate}
-          editTask={item}
-          edit={edit}
-          setEdit={setEdit}
-        />
-      )}
-    </TouchableOpacity>
-    </Swipeable>
+              <View style={styles.right}>
+                {item.isWork ? (
+                  <Octicons name="briefcase" size={30} color="pink" />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="gamepad-variant"
+                    size={33}
+                    color="turquoise"
+                  />
+                )}
+              </View>
+            </View>
+          )}
+          {edit && (
+            <TaskForm
+              setTriggerLoad={setTriggerLoad}
+              updateCalendar={true}
+              selectedDate={selectedDate}
+              editTask={item}
+              edit={edit}
+              setEdit={setEdit}
+            />
+          )}
+        </TouchableOpacity>
+      </Swipeable>
     </View>
-
   );
 }
 
@@ -157,46 +174,45 @@ export default CalendarItem;
 
 const styles = StyleSheet.create({
   touchable: {
-      flex: 1,
+    flex: 1,
   },
 
   container: {
     // backgroundColor: 'rgba(0, 0, 0, 0.05)',
     padding: 10,
     flex: 1,
-    flexDirection: 'row',
-    borderBottomLeftRadius: 10, 
+    flexDirection: "row",
+    borderBottomLeftRadius: 10,
     borderTopLeftRadius: 10,
-    borderStyle: 'solid', 
-    borderColor: 'black',
+    borderStyle: "solid",
     borderWidth: 1,
     // backgroundColor: 'white',
   },
 
   left: {
     flex: 0.7,
-  }, 
+  },
 
   right: {
     flex: 0.3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    alignItems: 'center'
-  }, 
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  },
 
   delete: {
     // marginTop: 15,
     zIndex: 0,
-    height: '100%',
-    backgroundColor: '#c95353',
-    justifyContent: 'center'
+    height: "100%",
+    backgroundColor: "#c95353",
+    justifyContent: "center",
   },
 
   buttonText: {
-    margin: '5%',
+    margin: "5%",
     zIndex: 1,
-    color: 'whitesmoke',
-    fontWeight: 'bold'
-  }
-})
+    color: "whitesmoke",
+    fontWeight: "bold",
+  },
+});
