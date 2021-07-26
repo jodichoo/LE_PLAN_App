b647ff25f3e27ed7b1d3288e1782760436189cc9
Shee Hui <tansheehui@gmail.com>
2021-07-26 13:26:44 +0800

    update onboading tour

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,4 +1,4 @@
-import { Image, Dimensions } from 'react-native';
+import { Image, Dimensions, View } from 'react-native';
 import React from 'react';
 import { useAuth } from "../navigation/AuthProvider";
 import Onboarding from 'react-native-onboarding-swiper';
@@ -58,41 +58,38 @@ function OnboardingScreen(props) {
             {
                 backgroundColor: '#680605',
                 image: <Image style={styles.welcome} source={require("../../assets/welcome.png")} />,
-                // title: 'Welcome to Le Plan',
-                // subtitle: 'pewpewpewpew',
+                title: '',
+                subtitle: '',
             },
             {
-                backgroundColor: '#fe6e58',
+                backgroundColor: '#680605',
                 image: <Image style={{width: width, height: width * 1080 / 756}} resizeMode={'contain'} source={require("../../assets/todaySchedule.gif")} />,
                 title: "Access Today's Schedule",
-                subtitle: 'Press on a task to view its description. Long press to edit the task',
+                subtitle: 'Make changes. Anytime. Anywhere.',
             },
             {
-                backgroundColor: '#fe6e58',
-                //access settings + profile here
+                backgroundColor: '#680605',
                 image: <Image style={{width: Dimensions.get('window').width * 0.65, height: Dimensions.get('window').width * 0.65 * 1056 / 600}} resizeMode={'contain'} source={require('../../assets/drawer.gif')} />,
-                title: 'View your Profile and Settings',
-                subtitle: 'This is the subtitle that sumplements the title.',
+                title: 'Swipe your hidden Drawer',
+                subtitle: 'View Profile, change themes and more! ◕。◕',
             },
             {
-                backgroundColor: '#fe6e58',
+                backgroundColor: '#680605',
                 image: <Image style={{width: width, height: width * 973 / 759}} resizeMode={'contain'} source={require('../../assets/calendar.gif')} />,
                 title: 'One Stop Platform to view all your plans',
-                subtitle: 'This is the subtitle that sumplements the title.',
+                subtitle: 'Update your Schedule on your convenience! ٩(̾●̮̮̃̾•̃̾)۶',
             },
             {
-                backgroundColor: 'whitesmoke',
-                //friends
-                //click to view friend profile
+                backgroundColor: '#680605',
                 image: <Image style={{width: width, height: width * 831 / 600}} resizeMode={'contain'} source={require("../../assets/friends.gif")} />,
                 title: 'Engage with your friends!',
-                subtitle: 'Click on your friend to view their profile',
+                subtitle: 'Peek at how your friends are doing this week ◕‿◕',
             },
             {
-                backgroundColor: '#333333',
-                // image: <Image style={styles.image} source={require('../../assets/defaultProfile.png')} />,
-                title: 'Get started!',
-                subtitle: "Press on the check button to get started!",
+                backgroundColor: '#680605',
+                image: (<View style={{width: '100%'}}/>),
+                title: 'Welcome',
+                subtitle: "",
             },
             ]}
         />
